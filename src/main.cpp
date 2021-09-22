62c9d4525a0a364c07ce0777c43c84970d02c157
gellidao <gellidao@gmail.com>
2021-09-21 19:40:09 -0500

    radio and Wifi are working together right

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,46 @@
 #include <Arduino.h>
 #include <Mathfunctions.h> 
 #include <WifiFunctions.h>
+#include <SPI.h>
+#include "printf.h"
+#include <RadioGamepad.h>
 
+Matrix<4, 1> qDeseado = {3.0, 3.0, 3.0, 3.0}; 
 
+Matrix<4, 1> qCurrent = {3.0, 3.0, 3.0, 3.0}; 
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-
-  // Inicializar I2C
-  //Wire.begin(); 
 
   delay(500) ; 
 
   // Inicializar WiFi y WebSocket
-
-  // Descomentar lineas de abajo en relación a si se tiene o no un modem disponible
+  //Descomentar lineas de abajo en relación a si se tiene o no un modem disponible
   //taskWifiNoModem() ; 
   taskWifiWithModem(); 
+
+  delay(500) ; 
+
+  InitialiceRadio();
 
 }
 
 
 void loop() {
 
-  CurrQuaternion = {1, 0, 0, 0}; 
+  recieved = recieve(qCurrent); 
 
-  DesiQuaternion = {1, 0, 0, 0}; 
+  sended = send(qDeseado) ; 
 
-  webSocket.loop();
+  if(sended){
+    CurrQuaternion = {1, 0, 0, 0}; 
 
-  getAndSendInfo() ; // Send all the info to the computer
+    DesiQuaternion = {1, 0, 0, 0}; 
 
-  delay(100); 
+    webSocket.loop();
+
+    getAndSendInfo() ; // Send all the info to the computer
+  }
+
+  
 }
