e10ff2eede84fdb67fc056e65c62a156c8a81fec
gellidao <gellidao@gmail.com>
2021-09-23 15:29:25 -0500

    Wifi, radio and IMU are workin excellent

--- NodeJScode/Server.js
+++ NodeJScode/Server.js
@@ -4,8 +4,6 @@ const WebSocket = require('ws');
 
 var index = fs.readFileSync('Web_visualization.html');
 
-const wss = new WebSocket.Server({ port: 8001 });
-
 
 var server = http.createServer(function (req, res) {   //create web server
     if (req.url == '/') { //check the URL of the current request
@@ -30,22 +28,24 @@ server.listen(5000); //6 - listen for any incoming requests
 
 console.log('Node.js web server at port 5000 is running..')
 
-wss.on('connection', function connection(ws) {
+const wss = new WebSocket.Server({ port: 8001 });
+
+
+ var WebSocketServer = require("ws").Server2;
   
-    ws.on('message', function incoming(message) {
-      //console.log('received: %s', message);
-      ws.send(message);
-    });
+  wss.on('connection', function connection(ws) {
+      ws.on('message', function(message) {
+         wss.broadcast(message);
+      }); 
   
-    ws.on('message', function(message) {
-      wss.broadcast(message);
-   }); 
-   
-  });
+  }) ;
+
   
   wss.broadcast = function broadcast(msg) {
-    //console.log(msg);
-    wss.clients.forEach(function each(client) {
-        client.send(msg);
-     });
-  }; 
+     //console.log(msg.toString());
+     wss.clients.forEach(function each(client) {
+         client.send(msg.toString());
+      });
+  };
+      
+   
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 /* Code created by Carlos Gellida, contact me at my email: gellidao@gmail.com */
 
 #include <Arduino.h>
-#include <Mathfunctions.h> 
+#include <Mathfunctions.h> // This library must be the second to do this program works
 #include <WifiFunctions.h>
 #include <SPI.h>
 #include "printf.h"
-#include <RadioGamepad.h>
+#include <RadioGamepad2.h>
 
-Matrix<4, 1> qDeseado = {3.0, 3.0, 3.0, 3.0}; 
+Matrix<4, 1> qDeseado = {1.0, 0.0, 0.0, 0.0}; 
 
-Matrix<4, 1> qCurrent = {3.0, 3.0, 3.0, 3.0}; 
+Matrix<4, 1> qCurrent = {1.0, 0.0, 0.0, 0.0}; 
+
+bool internetconection = true ;  // Do you want to use PC server? true = yes
+bool availableModem = true ; // Do you want to use ESP32 as an access point? true = No
+
 
 void setup() {
   Serial.begin(115200);
@@ -17,9 +21,14 @@ void setup() {
   delay(500) ; 
 
   // Inicializar WiFi y WebSocket
-  //Descomentar lineas de abajo en relación a si se tiene o no un modem disponible
-  //taskWifiNoModem() ; 
-  taskWifiWithModem(); 
+
+  if(internetconection){
+    if(availableModem){
+      taskWifiWithModem(); 
+    } else{
+      taskWifiNoModem() ; 
+    }
+  }
 
   delay(500) ; 
 
@@ -34,15 +43,13 @@ void loop() {
 
   sended = send(qDeseado) ; 
 
-  if(sended){
-    CurrQuaternion = {1, 0, 0, 0}; 
-
-    DesiQuaternion = {1, 0, 0, 0}; 
-
-    webSocket.loop();
-
-    getAndSendInfo() ; // Send all the info to the computer
+  if(internetconection){
+    // If you activate internect conection option: 
+    if(recieved){
+      // If messaje was recieved and sended correctly: 
+      webSocket.loop(); // Requested to run before to use web socket functions
+      getAndSendInfo(qCurrent, qDeseado) ; // Send all the info via Wifi to the computer
   }
-
+  }
   
 }
